c1ec949ca553ab1c392f6daa64171d791eb4f2a6
Luke Parker <lukep@digispend.io>
2018-06-07 05:28:44 -0500

    Switch to USB clock

--- include/tomu.h
+++ include/tomu.h
@@ -10,15 +10,6 @@
 #include <libopencm3/efm32/wdog.h>
 #include <libopencm3/efm32/cmu.h>
 
-//How often to cause a tick.
-//714 is roughly every 1 milisecond, however 700 is round.
-//It should be noted this isn't perfect as Tomus don't have crystals.
-//A variance of 0.05 seconds per second is expected.
-#define SYSTICK_FREQUENCY 700
-
-//Core clock frequency.
-#define AHB_FREQUENCY 14000000
-
 #include <toboot.h>
 TOBOOT_CONFIGURATION(0);
 
@@ -31,11 +22,9 @@ void setRun(void (*runArg)()) {
 }
 
 void sys_tick_handler() {
-    if (!(TomuHeader.run)) {
-        return;
+    if (TomuHeader.run != NULL) {
+        (*TomuHeader.run)();
     }
-
-    (*TomuHeader.run)();
 }
 
 void Tomu() {
@@ -45,13 +34,19 @@ void Tomu() {
 
     cmu_periph_clock_enable(CMU_GPIO);
 
-    systick_set_frequency(SYSTICK_FREQUENCY, AHB_FREQUENCY);
+    cmu_osc_on(USHFRCO);
+    cmu_wait_for_osc_ready(USHFRCO);
+    CMU_USBCRCTRL = CMU_USBCRCTRL_EN;
+    CMU_CMD = CMU_CMD_HFCLKSEL(5);
+    while (!(CMU_STATUS & CMU_STATUS_USHFRCODIV2SEL));
+
+    systick_set_frequency(1000, 24000000);
     systick_counter_enable();
     systick_interrupt_enable();
 }
 
 void loop() {
-    while(true){}
+    while (true);
 }
 
 #endif
